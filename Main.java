48d9e58679f9c40b0ade5488fad0497da33138f8
BrennanG0307 <brennang0307@5565.ca>
2020-09-12 17:19:46 -0600

    Fix to make sure it works

--- Main.java
+++ Main.java
@@ -4,8 +4,9 @@ class Main {
   public static void main(String[] args) {
   
   
-  String data = importer.importFile("covid19dataexport.txt");
-  importer.dataSplitter(data);
+  //String data = importer.importFile("covid19dataexport.txt");
+  //importer.dataSplitter(data);
+    System.out.println("Hello World");
   //This is a test
   }
 }
