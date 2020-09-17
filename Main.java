828068d23cf901b0e7b6218133e6c7bbce184d40
BrennanG0307 <brennang0307@5565.ca>
2020-09-17 09:54:03 -0600

    Graphics Update

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.*;
 
 class Main {
   public static void main(String[] args) {
   
   
-  String data = importer.importFile("covid19dataexport.txt");
-  importer.dataSplitter(data);
+  //String data = importer.importFile("covid19dataexport.txt");
+  //importer.dataSplitter(data);
+    JFrame frame = new JFrame("My Drawing");
+    Canvas canvas = new testGraphics();
+    canvas.setSize(400, 400);
+    frame.add(canvas);
+    frame.pack();
+    frame.setVisible(true);
   }
+  public void paint(Graphics g) {
+    g.setColor(Color.red);
+    g.fillOval(100, 100, 200, 200);
+    g.drawRect(100,100,100,100);
+  }
+
 }
